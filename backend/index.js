3f1762ec257139f6ce827d7c9ab6dc78467d8fb3
Yuval <yuvshul@gmail.com>
2024-07-17 13:08:55 +0300

    Add login in backend

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 const bcrypt = require('bcrypt')
 const User = require('./models/user')
 const Note = require('./models/note')
+const jwt = require('jsonwebtoken')
 
 const logFilePath = path.join(__dirname, 'log.txt')
 
@@ -181,14 +182,15 @@ app.get('/users', async (request, response) => {
 
 //Add a new user
 app.post('/users', async (request, response) => {
-  const { username, name, password } = request.body
+  const { name, email, username, password } = request.body
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
   const user = new User({
-    username,
     name,
+    email,
+    username,
     passwordHash,
   })
 
@@ -198,6 +200,34 @@ app.post('/users', async (request, response) => {
 })
 
 
+
+
+//LOGIN
+app.post('/login', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
 
 
 
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  username: String,
   name: String,
+  email: String,
+  username: String,
   passwordHash: String,
   notes: [
     {
